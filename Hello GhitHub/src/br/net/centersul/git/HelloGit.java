6236e45f9299b450f07ced3558be694307d8ab5f
msgrs <48494501+msgrs@users.noreply.github.com>
2019-07-12 15:33:34 -0300

    Adicionei arquivos via upload

---
+++ Hello GhitHub/src/br/net/centersul/git/HelloGit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.net.centersul.git;
+
+/**
+ * @author mgoulart
+ * Exemplo de integração do eclipse com o github
+ */
+public class HelloGit {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
